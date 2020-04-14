fc995610c23ce941d3da63985547418202c32e9e
Cyberon256 <go2ssemakula@gmail.com>
2020-04-14 16:08:51 +0300

    Reviewed req methods

--- api/routes.js
+++ api/routes.js
@@ -3,27 +3,24 @@ const xml2js = require('xml2js');
 const fs = require('fs');
 const estimator = require('./estimator');
 
-const startTime = null;
-const endTime = null;
 
 router.post('/', (req, res) => {
   res.status(200).send(estimator(req.body));
 });
 
 router.post('/:type', (req, res) => {
-  let myEstimate = estimator(req.body);
-
   if (req.params.type === 'xml') {
+    // convert estimator(req.body) to xml
     const builder = new xml2js.Builder({
       rootName: 'estimate',
       trim: true
     });
-    myEstimate = builder.buildObject(myEstimate);
-    res.type(​'​application/xml​'​);
+    const myEstimate = builder.buildObject(estimator(req.body));
+    res.type('application/xml');
     res.status(200).send(myEstimate);
   } else {
     // json
-    res.status(200).send(myEstimate);
+    res.status(200).send(estimator(req.body));
   }
 });
 
@@ -31,9 +28,11 @@ router.get('/logs', (req, res) => {
   // Read server.log
   fs.readFile('server.log', 'utf8', (err, file) => {
     if (err) {
-      // show the error
+      // console.log(err)
     } else {
-      res​.​type​(​'​text/plain​'​);
+      // return contents of the file
+      res.type('text/plain;charset=UTF-8');
+      // res.type('text/plain');
       res.status(200).send(file);
     }
   });
